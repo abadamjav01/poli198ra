cb7e4ce9ad399f8453784813c8b8e92c4bbaa5c5
abadamjav01 <abadamjav@ucsd.edu>
2026-04-15 16:27:40 -0700

    - created a new plot by aggregating policyautonomy variable w/ its respective different values. - removed all blatantly unnecessary variables(dummy variables, constant variables, some log based variables)

--- myscript.R
+++ myscript.R
@@ -9,6 +9,13 @@ rai = Conceptualizing_and_Measuring_Subnational_Democracy_Across_Indian_States_r
 democracy = rai
 rai = RAI_region_april_2021_final
 data = merge(rai, democracy, by = year)
-data = merge(rai, democracy, by = 'year')
 data
 savehistory("myscript.R")
+grouped_policyautonomy <- data
+grouped_policyautonomy %>% group_by(policyautonomy) %>% summarise(average = mean(enpl))
+grouped_policyautonomy <- grouped_policyautonomy %>% group_by(policyautonomy) %>% summarise(average = mean(enpl))
+ggplot(grouped_policyautonomy, aes(x=average, y=policyautonomy)) + geom_point(alpha=0.5) + geom_smooth(method="lm", se = FALSE) + labs(
+  +     title="Political Competition and Political Autonomy in India", x="Effective Number of Parties",
+  +     y="Political Autonomy")
+enpl_pa <- lm(policyautonomy ~ average, data = grouped_policyautonomy)
+summary(enpl_pa)
